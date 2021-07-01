4c8683e756297046d5ec5578b2c9b3d6f6e832a0
Toms Grinbergs <toms.grinbergs.lv@gmail.com>
2021-07-01 13:21:05 +0100

    Add support for vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,14 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import Vue from 'vue'
+
+const vueAppEls = document.querySelectorAll('[data-vue-app]')
+vueAppEls.forEach(vueAppEl => {
+    const app = require(`./components/${vueAppEl.dataset.vueApp}.vue`)
+    const props = vueAppEl.dataset.props ? JSON.parse(vueAppEl.dataset.props) : []
+    new Vue({
+        render: h => h(app.default, { props }),
+    }).$mount(vueAppEl)
+})
